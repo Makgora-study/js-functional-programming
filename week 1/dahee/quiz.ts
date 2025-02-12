b16444b003ed9069fe7c0b655a94539cb44b2716
Dahee Jo <74031552+daheejo@users.noreply.github.com>
2025-02-12 11:10:21 +0000

    chapter 1-5 문제

---
+++ week 1/dahee/quiz.ts
@@ -0,0 +1,7 @@
+const orders = [
+    { item: "Laptop", price: 1000, quantity: 2 },
+    { item: "Mouse", price: 25, quantity: 3 },
+    { item: "Keyboard", price: 50, quantity: 1 }
+  ];
+
+  //들어온 주문에 대한 총 합을 구하는 calculateTotalPrices 함수 만들기
